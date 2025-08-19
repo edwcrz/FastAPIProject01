6f47a7b1282756d113ab22e49e7faf604a0ca9d3
Eduardo Cruz <cruzeduardoa@gmail.com>
2025-08-19 17:11:35 -0300

    02 acomodo diccionario

--- Main.py
+++ Main.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI
-from typing import Union
-from pydantic import BaseModel
+# from typing import Union
+# from pydantic import BaseModel
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    surname: str
-    mail: str
-    age = int
+# class User(BaseModel):
+#    name: str
+#    surname: str
+#    mail: str
+#    age = int
 
-Users = User("Eduardo", "Cruz", "edwcrz@gmail.com", 35)
+# Users = User("Eduardo", "Cruz", "edwcrz@gmail.com", 35)
 
-@app.get("/userclass")
-async def userclass():
-    return Users(name = "Edu", surname = "Crz", mail = "edwcrz@gmail.com", age = 33)
+# @app.get("/userclass")
+# async def userclass():
+#     return Users(name = "Edu", surname = "Crz", mail = "edwcrz@gmail.com", age = 33)
+@app.get("/")
+async def root():
+    return  {"Hello" : "World FastAPI", 
+             "Mail" : "edwcrz@gmail.com", 
+             "Age" : 35
+            }
 
-# @app.get("/cv")
-# async def read_root():
-#    return  {"Name" : "Eduardo Cruz"}
-
+@app.get("/cv")
+async def get_cv():
+    return  {"Name" : "Eduardo Cruz", 
+             "Mail" : "cruzeduardoa@gmail.com", 
+             "Age" : 49
+            }
