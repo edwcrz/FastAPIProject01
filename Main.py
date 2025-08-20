94ce90332812831323e14c86ccc4e317f7d3a3c2
Eduardo Cruz <cruzeduardoa@gmail.com>
2025-08-20 12:51:21 -0300

    03 Cambio a instancia fastapi_Instance

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 # from typing import Union
 # from pydantic import BaseModel
 
-app = FastAPI()
+fastapi_Instance = FastAPI()
 
 # class User(BaseModel):
 #    name: str
@@ -15,14 +15,14 @@ app = FastAPI()
 # @app.get("/userclass")
 # async def userclass():
 #     return Users(name = "Edu", surname = "Crz", mail = "edwcrz@gmail.com", age = 33)
-@app.get("/")
+@fastapi_Instance.get("/")
 async def root():
     return  {"Hello" : "World FastAPI", 
              "Mail" : "edwcrz@gmail.com", 
              "Age" : 35
             }
 
-@app.get("/cv")
+@fastapi_Instance.get("/cv")
 async def get_cv():
     return  {"Name" : "Eduardo Cruz", 
              "Mail" : "cruzeduardoa@gmail.com", 
