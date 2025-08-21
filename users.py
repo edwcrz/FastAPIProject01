8b2aef7bd6cb46fd4421157e7dfe63a07b025c84
Eduardo Cruz <cruzeduardoa@gmail.com>
2025-08-21 18:03:59 -0300

    07 paso la version de json a /usersjson

--- users.py
+++ users.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
 
-@app.get("/users")
-async def users():
+class user():
+    id: int
+    name: str
+    surname: str
+    username: str
+    email: str
+
+@app.get("/usersjson")
+async def usersjson():
     return [{"id": 1, "name": "Eduardo", "surname": "Cruz", "email": "edwcrz@gmail.com"},
             {"id": 2, "name": "Edw", "surname": "Crz", "email": "edwcrz@gmail.com"},
             {"id": 3, "name": "edw", "surname": "crzdev", "email": "edwcrzdev@gmail.com"}]
